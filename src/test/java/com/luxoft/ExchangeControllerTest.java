e5eafa1e14d0f4f3395c35ae208fe48a46d9ea42
Michal Kordas <kordas.michal@gmail.com>
2016-09-01 11:46:53 +0200

    init commit with test

---
+++ src/test/java/com/luxoft/ExchangeControllerTest.java
@@ -0,0 +1,13 @@
+package com.luxoft;
+import org.junit.Assert;
+import org.junit.Test;
+public class ExchangeControllerTest {
+
+    private ExchangeController exchangeController = new ExchangeController();
+
+    @Test
+    public void testMessage(){
+        Assert.assertEquals("not equal","Name",exchangeController.hello("Name"));
+    }
+
+}
--- src/test/java/com/luxoft/HelloDemoApplicationTests.java
+++ src/test/java/com/luxoft/HelloDemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.luxoft;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class HelloDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 
 	}
 
